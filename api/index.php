f4f42d125a8ce25925da8849cc4789ae96383873
azkiaputri2907 <puteee63@gmail.com>
2026-02-17 20:23:28 +0800

    fix

--- api/index.php
+++ api/index.php
@@ -1,10 +1,3 @@
 <?php
-
-// 1. Inisialisasi database SQLite di folder writable Vercel
-$db = '/tmp/database.sqlite';
-if (!file_exists($db)) {
-    touch($db);
-}
-
-// 2. Load file index Laravel yang asli
+// Tidak perlu lagi touch database.sqlite
 require __DIR__ . '/../public/index.php';
