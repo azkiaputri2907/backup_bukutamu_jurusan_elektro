fddfbcfaa5ff0c314964ee8dbb58a9cfeb3f52e0
azkiaputri2907 <puteee63@gmail.com>
2026-02-17 20:09:11 +0800

    fix

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
 
-// 1. Inisialisasi file database di folder sementara Vercel
-$dbPath = '/tmp/database.sqlite';
-if (!file_exists($dbPath)) {
-    touch($dbPath);
+// 1. Inisialisasi database SQLite di folder writable Vercel
+$db = '/tmp/database.sqlite';
+if (!file_exists($db)) {
+    touch($db);
 }
 
-// 2. Jalankan Laravel melalui public/index.php
+// 2. Load file index Laravel yang asli
 require __DIR__ . '/../public/index.php';
