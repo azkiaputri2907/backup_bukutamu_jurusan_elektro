7fdac2076cbc0043886ec9b42b289608f11bf9f2
azkiaputri2907 <puteee63@gmail.com>
2026-02-17 19:13:26 +0800

    bismillah

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
+        tailwindcss(), // Ini wajib ada untuk Tailwind 4
         laravel({
-            input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-            ],
+            input: ['resources/sass/app.scss', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
